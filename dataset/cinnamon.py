21f0c9da9f538cae5f961d989747104d3e0e2ee8
linus <linus@cinnamon.is>
2019-07-01 11:51:58 +0700

    Prepare for custom dataset use

---
+++ dataset/cinnamon.py
@@ -0,0 +1,87 @@
+import os
+import random
+
+import cv2
+import numpy as np
+# from os.path import join
+import torch.utils.data as data
+# import matplotlib.pyplot as plt
+from PIL import Image, ImageFile
+from torchvision import transforms
+from torchvision.transforms import ToTensor
+
+
+class Cinnamon(data.Dataset):
+    """ Custom Cinnamon datasets class """
+
+    def __init__(self, root, ids, label=False, transforms=False):
+        super(Cinnamon, self).__init__()
+        """
+        Args:
+
+        fileDir(string):  directory with all the input images.
+        transform(callable, optional): Optional transform to be applied on a sample
+        """
+        self.root = root
+        self.label = label
+        self.transform = transform
+        self.ids = ids
+        self.classdict = {0: "background", 1: "text", 2: "table border"}
+        self.color_jitter = transforms.ColorJitter(
+            brightness=0.3, contrast=0.3, saturation=0.3, hue=0.04)
+        self.resizer = transforms.Resize((2448, 2448))
+
+    def __getitem__(self, index):
+        sample = {}
+        sample['id'] = self.ids[index][:-8]
+        image = Image.open(os.path.join(
+            self.root, "images/" + self.ids[index]))
+        sample['image'] = image
+        # TODO: Maybe need to create mask from json label or something
+        if self.label:
+            label = Image.open(os.path.join(
+                 self.root, 'labels/' + self.ids[index].replace('.jpg', '_mask.jpg')))
+            sample['label'] = label
+        if self.transform and self.label:
+            image, label = self._transform(image, label)
+            sample['image'] = image
+            sample['label'] = label
+        return sample
+
+
+    def _transform(self, image, label):
+        # if np.random.random() > 0.5:
+        #     image = self.color_jitter(image)
+
+        # if np.random.random() > 0.5:
+        #     image = transforms.functional.vflip(image)
+        #     label = transforms.functional.vflip(label)
+
+        if np.random.random() > 0.5:
+            image = transforms.functional.hflip(image)
+            label = transforms.functional.hflip(label)
+
+        if np.random.random() > 0.5:
+            degree = random.choice([90, 180, 270])
+            image = transforms.functional.rotate(image, degree)
+            label = transforms.functional.rotate(label, degree)
+
+        # if np.random.random() > 0.5:
+        #     degree = 60 * np.random.random() - 30
+        #     image = transforms.functional.rotate(image, degree)
+        #     label = transforms.functional.rotate(label, degree)
+
+        # if np.random.random() > 0.5:
+        #     ratio = np.random.random()
+        #     h = int(2448 * (ratio + 2) / 3.)
+        #     w = int(2448 * (ratio + 2) / 3.)
+        #     i = int(np.floor(np.random.random() * (2448 - h)))
+        #     j = int(np.floor(np.random.random() * (2448 - w)))
+        #     image = self.resizer(transforms.functional.crop(image, i, j, h, w))
+        #     label = self.resizer(transforms.functional.crop(label, i, j, h, w))
+        
+        return image, label
+
+
+    def __len__(self):
+        return len(self.ids)
---
+++ test_cinnamon.sh
@@ -0,0 +1,19 @@
+export CUDA_VISIBLE_DEVICES=0
+python train_cinnamon.py \
+--n_class 3 \
+--data_path "/home/linus/2DOCR/data/tadashi/" \
+--model_path "/home/linus/2DOCR/ultra_high_resolution_segmentation/experiments/default/" \
+--log_path "/home/linus/2DOCR/ultra_high_resolution_segmentation/experiments/default/" \
+--task_name "fpn_global.508_4.28.2019_lr2e5" \
+--mode 1 \
+--batch_size 6 \
+--sub_batch_size 6 \
+--size_g 508 \
+--size_p 508 \
+--path_g "fpn_global.resize512_9.2.2018.2.global.pth" \
+--path_g2l "fpn_global2local.508.deep.cat.1x_ensemble_fmreg.p3_10.14.2018.lr2e5.pth" \
+--path_l2g "fpn_local2global.508_deep.cat_ensemble.p3_10.31.2018.lr2e5.local1x.epoch13.pth" \
+--evaluation
+# --path_g "cityscapes_global.800_4.5.2019.lr5e5.pth" \
+# --path_g2l "fpn_global2local.508_deep.cat.1x_fmreg_ensemble.p3.0.15l2_3.19.2019.lr2e5.pth" \
+# --path_l2g "fpn_local2global.508_deep.cat.1x_fmreg_ensemble.p3_3.19.2019.lr2e5.pth" \
