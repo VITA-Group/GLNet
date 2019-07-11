35ba5dc55d1cd1e0d42ba85e2f233605d51b9f81
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-07-11 09:55:33 +0700

    Quick setup

---
+++ deep_globe_setup.sh
@@ -0,0 +1,12 @@
+git clone https://github.com/lamhoangtung/ultra_high_resolution_segmentation
+cd ultra_high_resolution_segmentation
+git checkout deepglobe
+mkdir data/
+cd data/
+drive clone 12UfVk6VkNsZjoEkF9ZB30buQmAc-HBKh
+unzip DeepGlobe_mod.zip
+cd ..
+mkdir experiments
+cd experiments
+mkdir deepglobe
+cd ../..
--- train_deep_globe.sh
+++ train_deep_globe.sh
@@ -1,9 +1,9 @@
 export CUDA_VISIBLE_DEVICES=0
-python train_deep_globe.py \
+python3 train_deep_globe.py \
 --n_class 7 \
---data_path "/ssd1/chenwy/deep_globe/data/" \
---model_path "/home/chenwy/deep_globe/FPN_based/github/saved_models/" \
---log_path "/home/chenwy/deep_globe/FPN_based/github/runs/" \
+--data_path "./data/DeepGlobe_mod/" \
+--model_path "./experiments/deepglobe/" \
+--log_path "./experiments/deepglobe/" \
 --task_name "fpn_global.508_4.28.2019_lr2e5" \
 --mode 1 \
 --batch_size 6 \
