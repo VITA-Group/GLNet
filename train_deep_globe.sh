4dcf1b043d1d4e1cf6de7badcd8f0e7e78a780aa
lamhoangtung <lamhoangtung.vz@gmail.com>
2019-08-05 16:49:58 +0700

    Minor

--- train_deep_globe.sh
+++ train_deep_globe.sh
@@ -13,6 +13,7 @@ python3 train_deep_globe.py \
 --path_g "fpn_global.resize512_9.2.2018.2.global.pth" \
 --path_g2l "fpn_global2local.508.deep.cat.1x_ensemble_fmreg.p3_10.14.2018.lr2e5.pth" \
 --path_l2g "fpn_local2global.508_deep.cat_ensemble.p3_10.31.2018.lr2e5.local1x.epoch13.pth" \
+--num_workers 8
 # --path_g "cityscapes_global.800_4.5.2019.lr5e5.pth" \
 # --path_g2l "fpn_global2local.508_deep.cat.1x_fmreg_ensemble.p3.0.15l2_3.19.2019.lr2e5.pth" \
 # --path_l2g "fpn_local2global.508_deep.cat.1x_fmreg_ensemble.p3_3.19.2019.lr2e5.pth" \
